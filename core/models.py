0ce5eb30033c95aef312bda849a3c7bfeb54db15
mdominguez2010 <mdominguez2010@yahoo.com>
2022-04-12 19:33:15 +0000

    finish models

--- core/models.py
+++ core/models.py
@@ -24,8 +24,16 @@ class Profile(models.Model):
 	
 
 class Movie(models.Model):
-	title = models.CharField(max_length = 225)
+	title: = models.CharField(max_length = 225)
 	description = models.TextField(blank = True, null = True)
 	created = models.DateTimeField(auto_now_add = True)
 	uuid = models.UUIDField(default = uuid.uuid4)
 	type = models.CharField(max_length = 10, choices = MOVIE_CHOICES)
+	videos = models.ManyToManyField('Video')
+	flyer = models.ImageField(upload_to = 'flyers')
+	age_limit = models.CharField(max_length = 10, choices = AGE_CHOICES)
+	
+	
+class Video(models.Model):
+	title:str = models.CharField(max_length = 225, blank = True, null = True)
+	file = models.FileField(upload_to = 'movies')
