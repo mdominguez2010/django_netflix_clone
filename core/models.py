db3b552eedd45eb6681629850c33f98e857240d8
mdominguez2010 <mdominguez2010@yahoo.com>
2022-04-12 19:22:37 +0000

    db setup

--- core/models.py
+++ core/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
 
-# Create your models here.
+AGE_CHOICES = (
+	('All', 'All'),
+	('Kids', 'Kids')
+)
+
+MOVIE_CHOICES = (
+	('seasonal', 'Seasonal'),
+	('single', 'Single')
+)
+
+
+class CustomUser(AbstractUser):
+	profiles = models.ManyToManyField("Profile", null = True, blank = True)
+	
+	
+class Profile(models.Model):
+	name = models.CharField(max_length = 225)
+	age_limit = models.CharField(max_length = 10, choices = AGE_CHOICES)
+	uuid = models.UUIDField(default = uuid.uuid4)
+	
+
+class Movie(models.Model):
+	title = models.CharField(max_length = 225)
+	description = models.TextField(blank = True, null = True)
+	created = models.DateTimeField(auto_now_add = True)
+	uuid = models.UUIDField(default = uuid.uuid4)
+	type = models.CharField(max_length = 10, choices = MOVIE_CHOICES)
