036c4375bf01f30f912e868f131b5a2a3ca1317e
Marcos D <md.ghsd@gmail.com>
2022-04-15 01:55:35 +0000

    update allowed hosts

--- django_netflix/settings.py
+++ django_netflix/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$d6%u-0b2yym)nqvd#^jxk@m@rqn8bfbhxh2*kz!tbivh9&-c0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['Djangonetflixclone-env.eba-4zbcam7y.us-west-1.elasticbeanstalk.com']
 
 
 # Application definition
