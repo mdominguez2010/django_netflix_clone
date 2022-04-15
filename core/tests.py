14b1abacddd62c1af9841b93d2f0251f38d351c0
Marcos D <md.ghsd@gmail.com>
2022-04-15 02:21:03 +0000

    update tests

--- core/tests.py
+++ core/tests.py
@@ -3,5 +3,5 @@ from django.test import TestCase
 class ViewTestCase(TestCase):
     def test_index_loads_properly(self):
         """The index page loads properly"""
-        response = self.client.get('https://127.0.0.1:8080/')
+        response = self.client.get('Djangonetflixclone-env.eba-4zbcam7y.us-west-1.elasticbeanstalk.com')
         self.assertEqual(response.status_code, 200)
