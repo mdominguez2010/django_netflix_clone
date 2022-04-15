2825156e7637a4e97c40b954da5ddb91a828710c
Marcos D <md.ghsd@gmail.com>
2022-04-15 02:07:58 +0000

    add test

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+class ViewTestCase(TestCase):
+    def test_index_loads_properly(self):
+        """The index page loads properly"""
+        response = self.client.get('https://127.0.0.1:8080/')
+        self.assertEqual(response.status_code, 200)
