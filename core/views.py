b984b69ecaf6aa8474afa2b55c1bafe4c615c840
Marcos D <mdominguez2010@yahoo.com>
2022-04-18 20:43:47 +0000

    add profileList page

--- core/views.py
+++ core/views.py
@@ -4,4 +4,12 @@ from django.views import View
 class Home(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'index.html')
-    
+        
+        
+class ProfileList(View):
+    def get(self, request, *args, **kwargs):
+        profiles = request.user.profiles.all()
+        return render(request, 'profileList.html', {
+            'profiles': profiles
+        }
+    )
