fb96976abef1d84fff817be6b35c8691b78a6d47
Marcos D <mdominguez2010@yahoo.com>
2022-04-19 19:09:39 +0000

    update profile list page

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import Home
+from .views import Home, ProfileList
 
 app_name = "core"
 
 urlpatterns = [
-	path('', Home.as_view())
+	path('', Home.as_view()),
+	path('profile/', ProfileList.as_view(), name = 'profile_list')
 ]
--- core/views.py
+++ core/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 class Home(View):
     def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect("core:profile_list")
         return render(request, 'index.html')
         
-        
+@method_decorator(login_required, name = "dispatch") 
 class ProfileList(View):
     def get(self, request, *args, **kwargs):
         profiles = request.user.profiles.all()
