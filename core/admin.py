da9633f2c1d457d44b43032e161d57c03dbb04c8
Marcos D <mdominguez2010@yahoo.com>
2022-04-20 18:59:19 +0000

    add profile/movie funcionality

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Movie, Profile, CustomUser, Video
 
-# Register your models here.
+admin.site.register(Movie)
+admin.site.register(Profile)
+admin.site.register(Video)
+admin.site.register(CustomUser)
+
+
