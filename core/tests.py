6e15c916db3f61c4818d3e78cd2015429736f4f6
Marcos D <mdominguez2010@yahoo.com>
2022-04-28 20:23:55 +0000

    add tests

--- core/tests.py
+++ core/tests.py
@@ -1,23 +1,25 @@
 from django.test import TestCase
 from django.test import SimpleTestCase
+from django.test import Client
 from django.urls import reverse, resolve
-from core import views
+
 
 class ViewTestCase(TestCase):
+    
     
     def test_index_loads_properly(self):
         """The index page loads properly"""
         response = self.client.get('https://127.0.0.1:8080/')
         self.assertEqual(response.status_code, 200)
-
-    def test_profileList_loads_properly(self):
-        """The profileList page loads properly"""
-        url = reverse("profile_list")
-        response = self.client.get(url)
+        
+    
+    def test_login_page_loads_properly(self):
+        """The login page loads properly"""
+        response = self.client.get('https://127.0.0.1:8080/accounts/login/')
         self.assertEqual(response.status_code, 200)
-    
-# class TestUrls(SimpleTestCase):
-    
-#     def test_profileList_url_is_resolved(self):
-#         url = reverse("profile_list")
-#         print(resolve(url))
+        
+        
+    def test_signup_page_loads_properly(self):
+        """The signup page loads properly"""
+        response = self.client.get('https://127.0.0.1:8080/accounts/signup/')
+        self.assertEqual(response.status_code, 200)
